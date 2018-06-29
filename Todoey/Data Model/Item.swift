edb89897ba900a2ede5167bad0cfeceafc6ba1e3
Jason Nelson <jason.nelson@jasons-mac-pro.local>
2018-06-29 10:50:03 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jason Nelson on 6/29/18.
+//  Copyright © 2018 Jason Nelson. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
